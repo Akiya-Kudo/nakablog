c0bc133f7e66b73f42de0dbeba3750082fa28e8c
Akiya-Kudo <k.akiya07@gmail.com>
2022-11-12 12:44:26 +0900

    button styled

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -6,9 +6,9 @@ import type { NextPage } from 'next'
 import styles from '../../styles/Home.module.css'
 import Menu, { MenuList } from '../../src/components/Menu'
 import Image from 'next/image';
-import Link from 'next/link';
+import NextLink from 'next/link'
 import { Dispatch, SetStateAction, useState } from "react";
-import { useArchive } from "../../src/hooks/useSortBlogs";
+import { useArchive, useTagBlogNumbers } from "../../src/hooks/useSortBlogs";
 import sanitizeHtml from 'sanitize-html';
 // ダイナミックSSGにおける型定義
 type BlogProp ={
@@ -51,12 +51,19 @@ export const getStaticProps = async(context: { params: { id: any; }; }) => {
     };
 };
 
+// コンポーネント
+const MenuSmallLink = ({text, number, setTag}: {text: pageStateType, number:String, setTag:Dispatch<SetStateAction<pageStateType>>}) => {
+    return (
+            <NextLink href={"/"} legacyBehavior passHref>
+                <div  className={ styles.link } onClick={() => setTag(text)}>
+                    <MenuSmallButton text={text}  number={number} />
+                </div>
+            </NextLink>
+  )}
 
 // コンポーネント
 const MenuSmallButton = ({text , bgcolor, number}: {text: string, bgcolor?: string, number:String}) => {
     return (
-      <Box href="" legacyBehavior className={ styles.link }>
-        <CLink as="a" className={ styles.link }>
             <HStack left="0px" mx={5} color={"gray"} pos={"relative"} _hover={{ left: "3px", color:"blackAlpha.900", transition: "0.5s" }}  fontSize={15} fontWeight="bold" borderRadius={5} className={styles.linkSmall}>
                 <Text  mx={5}  bg={bgcolor} h={30}  borderRadius={10}>
                     {text}
@@ -65,9 +72,9 @@ const MenuSmallButton = ({text , bgcolor, number}: {text: string, bgcolor?: stri
                     {number}
                 </Center>
             </HStack>
-        </CLink>
-      </Box>
   )}
+
+
 // ページコンポーネント
 export default function BlogId({blog, blogs, isTag, setTag}:{blog: Blog,blogs:Blog[], isTag:pageStateType, setTag:Dispatch<SetStateAction<pageStateType>>}){
     const dateprot = blog.createdAt.substring( 0, 10 ) ;
@@ -75,11 +82,11 @@ export default function BlogId({blog, blogs, isTag, setTag}:{blog: Blog,blogs:Bl
     // 月別アーカイブ
     const archive = useArchive(blogs)
 
+    // カテゴリー
+    const categoryNum = useTagBlogNumbers(blogs)
+    console.log(categoryNum)
 
-    
     const headingCorrectedBlog = blog.contents.replace( /<h1/g, '<h1 style="font-size: 35px; font-weight: bolder" ').replace( /<h2/g, '<h2 style="font-size: 25px; font-weight: bolder" ').replace( /<h3/g, '<h3 style="font-size: 20px; font-weight: bolder" ').replace( /<h4/g, '<h4 style="font-size: 15px; font-weight: bolder" ').replace( /<h5/g, '<h5 style="font-size: 13px; font-weight: bolder" ');
-    
-    // console.log(blog)
     // const sanitizedContent = sanitizeHtml(blog.contents , { allowedTags: ["p", "br" , "img", "a", "strong", "em", "s", "code", "span","h6","h5","h4","h3","h2","h1","li", "pre", "ul", "sup","sub"], allowedAttributes: { button: ['class'], img: [ 'src', 'srcset', 'alt', 'title', 'width', 'height', 'loading' ], p: ["style"], a: ["href"]},disallowedTagsMode: 'escape', });
 
     return(
@@ -119,33 +126,39 @@ export default function BlogId({blog, blogs, isTag, setTag}:{blog: Blog,blogs:Bl
                         display={{base: "block", md: "none"}}
                         colSpan={1} bg={"back.100"} borderRadius={10} boxShadow="md" p={3}
                         >
-                            <Text mx={5} mt={2} mb={2} fontSize={15} fontWeight="bold" borderBottom={"2px solid #a5e8cb"}>カテゴリー</Text>
-                            <MenuSmallButton text="イベント"/>
-                            <MenuSmallButton text="日常"/>
-                            <MenuSmallButton text="研究"/>
-                            <MenuSmallButton text="その他"/>
+                            <Text mx={5} mt={2} mb={2} fontSize={15} fontWeight="bold" borderBottom={"2px solid #ffbb8a"}>カテゴリー</Text>
+                            <MenuSmallLink text="イベント" number={categoryNum.eventBlogs.toString()} setTag={ setTag }/>
+                            <MenuSmallLink text="日常" number={categoryNum.dailyBlogs.toString()} setTag={ setTag }/>
+                            <MenuSmallLink text="研究" number={categoryNum.studyBlogs.toString()} setTag={ setTag }/>
+                            <MenuSmallLink text="その他" number={categoryNum.otherBlogs.toString()} setTag={ setTag }/>
                         </GridItem>
                         <GridItem
                         colSpan={1} bg={"back.100"} borderRadius={10} boxShadow="md" p={3}
                         >
-                            <Text mx={5} mt={2} mb={2} fontSize={15} fontWeight="bold" borderBottom={"2px solid #a5e8cb"}>アーカイブ</Text>
+                            <Text mx={5} mt={2} mb={2} fontSize={15} fontWeight="bold" borderBottom={"2px solid #ffbb8a"}>アーカイブ</Text>
                             <Accordion defaultIndex={[0]} allowMultiple>
                                 {Object.keys(archive).map((index)=>(
                                     <AccordionItem>
                                         <h2>
-                                            <AccordionButton>
+                                            <AccordionButton _hover={{bg:"whiteAlpha.900"}}>
                                                 <Box flex='1' textAlign='left'>
-                                                    <MenuSmallButton  number={archive[index].length}  text={index.split("_")[0] + "年" + index.split("_")[1] + "月"} />
+                                                    <MenuSmallButton  number={archive[index].length}  text={index.split("_")[0] + "年" + index.split("_")[1] + "月"}/>
                                                 </Box>
                                                 <AccordionIcon />
                                             </AccordionButton>
                                         </h2>
                                         {archive[index].map((blog: Blog)=>(
-                                            <AccordionPanel pb={4} textAlign='center'>
-                                                <Link href={`/blog/${blog.id}`}>
-                                                        {blog.title}
-                                                        <Tag>{blog.tag}</Tag>
-                                                </Link>
+                                            <AccordionPanel pb={2} textAlign='center'>
+                                                <NextLink href={`/blog/${blog.id}`}>
+                                                    <HStack left="0px" mx={5} color={"gray"} pos={"relative"} _hover={{ left: "3px", color:"blackAlpha.900", transition: "0.5s" }}  fontSize={15} fontWeight="bold" borderRadius={5} className={styles.linkSmall}>
+                                                        <Text  mx={2}  h={30}  borderRadius={10}>
+                                                            {blog.title}
+                                                        </Text>
+                                                        <Center pos={"absolute"} right={5} border="1px solid #009b85"  fontSize={13} fontWeight="500" px={1} borderRadius={5} color="base.700">
+                                                            {blog.tag}
+                                                        </Center>
+                                                    </HStack>
+                                                </NextLink>
                                             </AccordionPanel>
                                         ))}
                                     </AccordionItem>
